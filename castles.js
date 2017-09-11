753782d627bcdb9f32a718a37416909e7bdce0e1
Frank Clarke <fclarke@shaw.ca>
2017-09-11 16:37:27 -0700

    Special case for last index
    "If I told you that the input 2,2,4,4,5,5,5,-1,3,3 is supposed to produce an output of 4 (first element, peak of height 5, valley of height -1 and peak of height 3), how would you change your code?"

--- castles.js
+++ castles.js
@@ -1,53 +1,73 @@
 /*jslint browser: true */
-function randomArray(length, max) {
+function randomArray(length, max, offset) {
     "use strict";
     return Array.apply(null, Array(length)).map(function () {
-        return Math.round(Math.random() * max) + 1;
+        return (Math.round(Math.random() * max) + 1) + offset;
     });
 }
 
+var firstTime = true;
+
 function startCastles() {
     "use strict";
-    var max = 10;
-    var myArray = randomArray(10, max - 1);
-    //myArray = [9, 8, 7, 4, 7, 7, 4, 3, 4, 5];
+    var length = 10,
+        offset = -2;
+    var myArray;
+
     var color,
         plateau = 0,
         downSlope = 0,
         left, right, current,
         upSlope = 0;
 
-    for (var i = 0; i < max; i++) {
+    if (firstTime) {
+        myArray = [2, 3, 0, 4, 5, 5, 5, -1, 3, 3];
+        //myArray = [2, 8, 6, 1, 4, 3, 8, 4, 7, 6];
+        firstTime = false;
+    } else {
+        myArray = randomArray(10, length - 1, offset)
+    }
+
+    document.getElementById('chart').innerHTML = "";
+    document.getElementById('numbers').innerHTML = "";
+
+    for (var i = 0; i < length; i++) {
+
         color = "chart--default";
 
-        left = myArray[i + 1];
-        right = myArray[i - 1];
+        left = myArray[i - 1];
+        right = myArray[i + 1];
         current = myArray[i];
-
-        if (i != max - 1) {
-            if (current > right)
-                if (current < left) {
-                    upSlope++;
-                    color = "chart--blue";
-                }
-            if (current < right)
-                if (current > left) {
-                    downSlope++;
-                    color = "chart--green";
-                }
-        }
-
-        if (current === left) {
-            plateau++;
+        if (i == 0) {
             color = "chart--yellow";
         }
+        if (i != length - 1) {
+            if (current > right) {
+                upSlope = true;
+                if (downSlope) {
+                    color = "chart--blue";
+                    downSlope = false;
+                }
+            }
+            if (current < right) {
+                downSlope = true;
+                if (upSlope) {
+                    color = "chart--green";
+                    upSlope = false;
+                }
+            }
+        } else {
+            color = "chart--default";
+            if (upSlope)
+                color = "chart--green";
+            if (downSlope)
+                color = "chart--blue";
+        }
 
-        document.getElementById('chart').innerHTML += getHtml(myArray[i] * 10, color);
+        document.getElementById('chart').innerHTML += getHtml((myArray[i] - offset) * 10, color);
         document.getElementById('numbers').innerHTML += myArray[i] + " ";
+        document.getElementById('key').innerHTML = ("<span style='background-color:#F1C40F' >Initial " + "</span> <span style='background-color:#2980B9' >Peak " + "</span>  <span style='background-color:#27AE60' >Valley" + "</span>");
     }
-    document.getElementById('plateau').innerHTML = ("<span style='background-color:#F1C40F' >plateau " + plateau + "</span> <span style='background-color:#2980B9' >up " + upSlope + "</span>  <span style='background-color:#27AE60' >down " + downSlope + "</span>");
-    document.getElementById('castles').innerHTML = "Castles = " + max + " - " + plateau + " - " + upSlope + " - " + downSlope + " = " +
-        (max - plateau - upSlope - downSlope);
 }
 
 
